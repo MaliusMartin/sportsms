bbca1310dd6b9f67954646be3c017fe2871261b1
MaliusMartin <maliberry7@gmail.com>
2023-11-11 10:25:43 +0300

    middleware

--- sportsms/settings.py
+++ sportsms/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "django_browser_reload.middleware.BrowserReloadMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sportsms.urls'
